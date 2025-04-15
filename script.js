805267f1493edbb0931795e6fa677cf5a76638b6
Guilherme Cardoso <guilherme.cardoso@sksoft.pt>
2025-04-15 19:11:06 +0100

    alteracao scheme color e datepicker

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 document.addEventListener('DOMContentLoaded', function() {
     var currentYear = new Date().getFullYear();
-    // document.getElementById('currentYear').textContent = currentYear;
+    document.getElementById('currentYear').textContent = currentYear;
     carousel();
+
+    flatpickr("#scheduleDate", {
+      dateFormat: "d-m-Y",
+    });
 });
 
 function Schedule() {
@@ -19,55 +23,10 @@ function carousel() {
   var x = document.getElementsByClassName("slide");
   for (i = 0; i < x.length; i++) {
     x[i].classList.remove("active");
-    // x[i].classList.remove("transform");
   }
   myIndex++;
   if (myIndex > x.length) {myIndex = 1}    
   x[myIndex-1].classList.add("active");
-//   x[myIndex-1].classList.add("transform");
   setTimeout(carousel, 5000); // Change image every 5 seconds
 }
 
-// function ShowSection(section) {
-//     if (section == "contacts") {
-//         $("#" + section).css("opacity", "1");
-//         $("#" + section).css("z-index", "10");
-//         $(".showSection").css("opacity", "0.3");
-//     }
-//     else if (section == "portfolio") {
-//         // $(".header").remove();
-//         $(".header").css("opacity", "0");
-//         $("header").css("opacity", "1");
-
-//         $(".showSection").removeAttr('style');
-//         $("#contacts").css("opacity", "0");
-
-//         $("section").removeClass("showSection");
-//         $("#" + section).addClass("showSection");
-//     }
-//     else if (section == "about") {
-//         // $(".header").remove();
-//         $(".header").css("opacity", "0");
-//         $("header").css("opacity", "1");
-        
-//         $(".showSection").removeAttr('style');
-//         $("#contacts").css("opacity", "0");
-
-//         $("section").removeClass("showSection");
-//         $("#" + section).addClass("showSection");
-//     }
-// }
-
-// function HideSection(section) {
-//     $("#" + section).css("opacity", "0");
-//     $(".showSection").css("opacity", "1");
-//     $(".showSection").removeAttr('style');
-// }
-
-// function HomePage() {
-//     $(".showSection").removeAttr('style');
-//     $("section").removeClass("showSection");
-
-//     $("header").css("opacity", "0");
-//     $(".header").css("opacity", "1");
-// }
